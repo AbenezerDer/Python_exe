f97aeb5da483d6159b7162849d4e7437195ff23f
AbenezerDer <dabenu2012@gmai.com>
2022-12-16 11:01:03 +0300

    Looping.py

--- Looping.py
+++ Looping.py
@@ -6,3 +6,15 @@ for  i in range(1, 21):
 
 # print the odds
     print("i = ", i)
+    
+# Have the user enter their investment amount and expected interest
+# Each year their investment will increase by their investment + their investment * interest rate is 
+# print out the earnigns after a 10 year period 
+
+# Ask for the money invested + the interest rate 
+
+# convert the value to a float
+
+#
+
+    
