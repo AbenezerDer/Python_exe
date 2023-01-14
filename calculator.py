8933d06ec85611f63ffb8bd55bd413b16eaa2aaa
AbenezerDer <dabenu2012@gmail.com>
2023-01-14 11:47:25 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -1,6 +1,9 @@
+# we use input for the user to insert an input
+
 x = input("what's x? ")
 y = input("what's y? ")
 
+# we use int for thr py to detect (x) is a number not avariable in this case 
 z = int(x) + int(y)
 print(z)
 
@@ -9,5 +12,7 @@ print(z)
 x = input("what's x? ")
 y = input("what's y? ")
 
+# we use round for the system python to choose weather it is float or integer 
+# we use f to insert the , or . after three zeros 
 z = round(x + y)
-print(z)
+print(f"{z:,}")
