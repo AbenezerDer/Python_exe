52057bea7b9c9a55db56e7d6af07eb243b9d723f
AbenezerDer <dabenu2012@gmail.com>
2023-01-14 11:36:03 +0300

    calculator

--- calculator.py
+++ calculator.py
@@ -1,5 +1,13 @@
-x = 1
-y = 2
+x = input("what's x? ")
+y = input("what's y? ")
 
-z = x + y
+z = int(x) + int(y)
+print(z)
+
+# float
+
+x = input("what's x? ")
+y = input("what's y? ")
+
+z = round(x + y)
 print(z)
